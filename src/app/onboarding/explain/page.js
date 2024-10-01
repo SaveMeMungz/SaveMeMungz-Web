012868599e4388cc701edae2bc5512823367699e
InKyungWoo <chelsea1201@naver.com>
2024-10-01 19:54:01 +0900

    remove: 불필요한 layout 파일 삭제 (#1)

--- src/app/onboarding/explain/page.js
+++
@@ -1,9 +0,0 @@
-// src/app/onboarding/explain/page.js
-export default function ExplainOnboardingPage() {
-    return (
-        <div>
-            <h2>Explanation of the onboarding process</h2>
-            <p>Here is how you can start using our service...</p>
-        </div>
-    );
-}
--- src/app/onboarding/start/page.js
+++
@@ -1,9 +0,0 @@
-// src/app/onboarding/start/page.js
-export default function StartOnboardingPage() {
-    return (
-        <div>
-            <h2>Start Onboarding</h2>
-            <p>Begin your onboarding journey here...</p>
-        </div>
-    );
-}
