3f00b5d99f4aa52c4f92576a7815277148faec7f
InKyungWoo <chelsea1201@naver.com>
2024-10-04 17:51:01 +0900

    style: color, space 상수 파일 추가 (#4)

---
+++ src/constants/space.js
@@ -0,0 +1,19 @@
+export const SPACING = {
+    xxs: '0.25rem', // 4px
+    xs: '0.5rem', // 8px
+    sm: '0.75rem', // 12px
+    md: '1rem', // 16px
+    lg: '1.25rem', // 20px
+    xl: '1.5rem', // 24px
+    xxl: '3rem', // 48px
+};
+
+export const PADDING_HORIZONTAL = {
+    paddingLeft: `${SPACING.xl}`,
+    paddingRight: `${SPACING.xl}`,
+};
+
+export const PADDING_VERTICAL = {
+    paddingTop: `${SPACING.md}`,
+    paddingBottom: `${SPACING.md}`,
+};
