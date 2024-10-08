6b728aa2183c7ce26807cd1a6ed939358ce1795e
InKyungWoo <chelsea1201@naver.com>
2024-10-09 03:33:38 +0900

    feat: 회원 정보 수정 페이지 라우팅 설정 (#9)

--- src/app/(main)/mypage/edit/page.jsx
+++ src/app/(main)/mypage/edit/page.jsx
@@ -1,14 +1,28 @@
+'use client';
+
+import styled from 'styled-components';
+
 import CustomHeader from '../../../../components/layout/CustomHeader';
+import { BACKGROUND_COLORS } from '../../../../constants/colors';
 
 const EditInfo = () => {
     return (
-        <>
-            <CustomHeader title="회원 정보 수정" />
-            <div>
+        <Container>
+            <CustomHeader title="회원 정보 수정" showBackBtn={true} />
+            <ContentContainer>
                 <h2>정보 수정 페이지</h2>
-            </div>
-        </>
+            </ContentContainer>
+        </Container>
     );
 };
 
 export default EditInfo;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const ContentContainer = styled.div`
+    border-top: 1px solid ${BACKGROUND_COLORS.divider};
+`;
