5b569e295348e97dbb8854a64771a6d69ab74014
InKyungWoo <chelsea1201@naver.com>
2024-10-12 04:20:47 +0900

    [ISSUE-14] design: 메인 컨텐츠 영역 padding-bottom 수정 (#22)

--- src/app/(main)/course/page.jsx
+++ src/app/(main)/course/page.jsx
@@ -1,17 +1,26 @@
+'use client';
+
+import styled from 'styled-components';
+
 import InfoBanner from '../../../components/common/InfoBanner';
 import CustomHeader from '../../../components/layout/CustomHeader';
+import MapView from './MapView';
 
 const Course = () => {
     return (
-        <>
+        <Container>
             <CustomHeader title="보호소별 산책 코스 추천" />
             <InfoBanner message="지역 필터 들어갈 곳" />
-            <div>
-                <h2>Recommended Walking Routes</h2>
-                <p>Explore nearby walking routes for you and your dog...</p>
-            </div>
-        </>
+            {/* 지도 영역 */}
+            <MapView />
+        </Container>
     );
 };
 
 export default Course;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+`;
