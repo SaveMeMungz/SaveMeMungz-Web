d02f05a7d5134c2d878483f15fdf3aa3729a853f
InKyungWoo <chelsea1201@naver.com>
2024-10-12 08:05:36 +0900

    [ISSUE-14] feat: 산책코스 페이지 퍼블리싱 (#22)

--- src/app/(main)/course/page.jsx
+++ src/app/(main)/course/page.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import CustomHeader from '../../../components/layout/CustomHeader';
 import FilterBarComponent from './FilterBarComponent';
 import MapView from './MapView';
+import ShelterCardComponent from './ShelterCardComponent';
 
 const Course = () => {
     return (
@@ -13,7 +14,12 @@ const Course = () => {
             {/* 보호소 선택 필터 */}
             <FilterBarComponent />
             {/* 지도 영역 */}
-            <MapView />
+            <MapContainer>
+                <MapView />
+            </MapContainer>
+            <CardWrapper>
+                <ShelterCardComponent />
+            </CardWrapper>
         </Container>
     );
 };
@@ -23,5 +29,18 @@ export default Course;
 const Container = styled.div`
     display: flex;
     flex-direction: column;
-    height: 100%;
+    height: calc(100vh - var(--bottom-tab-height));
+`;
+
+const MapContainer = styled.div`
+    flex: 1;
+    position: relative;
+`;
+
+const CardWrapper = styled.div`
+    position: absolute;
+    bottom: calc(var(--bottom-tab-height) + 1.25rem);
+    left: 0;
+    right: 0;
+    z-index: 10;
 `;
