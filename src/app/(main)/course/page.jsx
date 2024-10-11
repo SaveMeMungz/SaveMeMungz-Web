aa79d6f4b0289f9b3865f51d8f9d2de0bcc55f59
InKyungWoo <chelsea1201@naver.com>
2024-10-12 06:24:35 +0900

    [ISSUE-14] feat: 지역, 보호소 필터 컴포넌트 구현 (#22)

--- src/app/(main)/course/page.jsx
+++ src/app/(main)/course/page.jsx
@@ -2,15 +2,16 @@
 
 import styled from 'styled-components';
 
-import InfoBanner from '../../../components/common/InfoBanner';
 import CustomHeader from '../../../components/layout/CustomHeader';
+import FilterBarComponent from './FilterBarComponent';
 import MapView from './MapView';
 
 const Course = () => {
     return (
         <Container>
             <CustomHeader title="보호소별 산책 코스 추천" />
-            <InfoBanner message="지역 필터 들어갈 곳" />
+            {/* 보호소 선택 필터 */}
+            <FilterBarComponent />
             {/* 지도 영역 */}
             <MapView />
         </Container>
