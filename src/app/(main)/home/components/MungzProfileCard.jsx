45aa7885a2f6ab452e179a46783a719b8f309657
InKyungWoo <chelsea1201@naver.com>
2024-10-13 17:05:48 +0900

    [ISSUE-32] refactor: 멍즈 프로필 카드 컴포넌트 리팩토링 및 css 수정 (#26)

--- src/app/(main)/home/components/MungzProfileCard.jsx
+++ src/app/(main)/home/components/MungzProfileCard.jsx
@@ -1,7 +1,9 @@
 'use client';
 
+import Image from 'next/image';
 import styled from 'styled-components';
 
+import { TEXT_COLORS } from '../../../../constants/colors';
 import { FONTS } from '../../../../constants/font';
 
 const MungzProfileCard = ({ subtitle, profileData, isLoading, error }) => {
@@ -10,13 +12,15 @@ const MungzProfileCard = ({ subtitle, profileData, isLoading, error }) => {
     if (!profileData) return <div>데이터가 없습니다.</div>;
 
     return (
-        <RecommendationWrapper>
-            <RecommendTitleRow>
-                <TodayRecommendMungz>{subtitle}</TodayRecommendMungz>
-            </RecommendTitleRow>
-            <RecommendProfileContainer>
-                <ProfilePic src={profileData.image} />
-                <InfoRowContainerWrapper>
+        <RecomendationWrapper>
+            <Subtitle>{subtitle}</Subtitle>
+            {/* 멍즈 카드 */}
+            <CardContainer>
+                <ProfilePicWrapper>
+                    <Image src={profileData.image} alt="멍즈 프로필 사진" fill style={{ objectFit: 'cover' }} />
+                </ProfilePicWrapper>
+                {/* 정보 */}
+                <InfoWrapper>
                     <InfoColumn>
                         <InfoItem label="지역" data={profileData.region} />
                         <InfoItem label="품종" data={profileData.breed} />
@@ -24,14 +28,14 @@ const MungzProfileCard = ({ subtitle, profileData, isLoading, error }) => {
                         <InfoItem label="체중" data={profileData.weight} />
                     </InfoColumn>
                     <InfoColumn>
-                        <InfoItem label="보호소명" data={profileData.shelterName} />
-                        <InfoItem label="연락처" data={profileData.contact} />
-                        <InfoItem label="중성화" data={profileData.neutered} />
-                        <InfoItem label="특징" data={profileData.characteristic} />
+                        <InfoItem2 label="보호소명" data={profileData.shelterName} />
+                        <InfoItem2 label="연락처" data={profileData.contact} />
+                        <InfoItem2 label="중성화" data={profileData.neutered} />
+                        <InfoItem2 label="특징" data={profileData.characteristic} />
                     </InfoColumn>
-                </InfoRowContainerWrapper>
-            </RecommendProfileContainer>
-        </RecommendationWrapper>
+                </InfoWrapper>
+            </CardContainer>
+        </RecomendationWrapper>
     );
 };
 
@@ -42,83 +46,100 @@ const InfoItem = ({ label, data }) => (
     </DetailRow>
 );
 
+const InfoItem2 = ({ label, data }) => (
+    <DetailRow>
+        <Label2>{label}</Label2>
+        <Data2>{data}</Data2>
+    </DetailRow>
+);
+
 export default MungzProfileCard;
 
 // 스타일링
-const RecommendationWrapper = styled.div`
+const RecomendationWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    width: 24.375rem;
-    padding: 1rem 1.5rem;
     gap: 0.5rem;
 `;
 
-const RecommendTitleRow = styled.div``;
-
-const TodayRecommendMungz = styled.div`
-    color: #204b5a;
+const Subtitle = styled.div`
+    color: ${TEXT_COLORS.title};
     font-family: ${FONTS.PRETENDARD[700]};
     font-size: 1.125rem;
 `;
 
-const RecommendProfileContainer = styled.div`
+const CardContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    gap: 1.5rem;
+    border: 1px solid ${TEXT_COLORS.grey};
     border-radius: 0.5rem;
-    border: 1px solid #a5a5a5;
-    box-shadow: 3px 3px 4px rgba(0, 0, 0, 0.5);
-    width: 21.375rem;
-    padding: 1rem; /* padding을 사용해 내부 여백 추가 */
-    min-height: 18rem; /* 최소 높이 설정 */
-`;
-
-const ProfilePic = styled.div`
-    width: 8.125rem;
-    height: 8.125rem;
-    background-color: #eee;
-    border-radius: 50%;
-    background-image: url(${(props) => props.src});
-    background-size: cover;
-    background-position: center;
-`;
-
-const InfoRowContainerWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
+    box-shadow: 3px 4px 4px 1px rgba(77, 77, 77, 0.5);
+    padding: 1rem 1.5rem;
     gap: 1.5rem;
+`;
+
+const ProfilePicWrapper = styled.div`
+    width: 9.375rem;
+    height: 9.375rem;
+    width: 8.75rem;
+    height: 8.75rem;
+    position: relative;
+    border-radius: 50%;
+    overflow: hidden;
+    box-shadow: 3px 4px 4px 0px rgba(71, 152, 122, 0.5);
+`;
+
+const InfoWrapper = styled.div`
+    display: flex;
+    gap: 1rem;
 `;
 
 const InfoColumn = styled.div`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
-    width: 6rem;
-    flex-shrink: 0;
 `;
 
 const DetailRow = styled.div`
     display: flex;
-    gap: 0.7rem;
+    gap: 0.5rem;
 `;
 
-const Label = styled.span`
-    color: #347d6d;
+// 공통 스타일 정의
+const LabelStyles = `
+    color: ${TEXT_COLORS.default};
     font-family: ${FONTS.PRETENDARD[700]};
     font-size: 0.75rem;
-    width: 2rem; /* 고정된 너비 */
-    flex-shrink: 0;
     white-space: nowrap; /* 줄바꿈 방지 */
 `;
 
-const Data = styled.span`
-    color: #347d6d;
+const DataStyles = `
+    color: ${TEXT_COLORS.default};
     font-family: ${FONTS.PRETENDARD[400]};
     font-size: 0.75rem;
     white-space: nowrap; /* 텍스트 줄바꿈을 방지 */
     overflow: hidden; /* 넘치는 텍스트를 숨김 */
     text-overflow: ellipsis; /* 넘치는 텍스트를 ...으로 표시 */
-    max-width: 100px; /* 적절한 최대 너비를 설정 */
+`;
+
+const Label = styled.span`
+    ${LabelStyles}
+    width: 1.75rem; /* 고정된 너비 */
+`;
+
+const Data = styled.span`
+    ${DataStyles}
+    width: 5rem;
+`;
+
+const Label2 = styled.span`
+    ${LabelStyles}
+    width: 2.8125rem;
+`;
+
+const Data2 = styled.span`
+    ${DataStyles}
+    width: 6.25rem;
 `;
