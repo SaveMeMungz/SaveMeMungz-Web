6f6fd9dea00ae0eabd58f208a44d97ca58315a2b
Kimjaewoo <kimjaewoo@hufs.ac.kr>
2024-10-23 18:41:09 +0900

    [ISSUE-45] feat: sign-up 페이지 제작 시작 (#31)

--- src/app/(auth)/sign-up/page.jsx
+++ src/app/(auth)/sign-up/page.jsx
@@ -1,14 +1,84 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import styled from 'styled-components';
+
+import whitepawImg from '../../../assets/images/white-paw.png';
+import CustomButton from '../../../components/common/CustomButton';
+import { BACKGROUND_COLORS, BUTTON_COLORS, TEXT_COLORS } from '../../../constants/colors';
+import { FONTS } from '../../../constants/font';
+import { PADDING_HORIZONTAL, PADDING_VERTICAL } from '../../../constants/space';
+
 const SignUp = () => {
     return (
-        <div>
-            <h1>SignUp Page</h1>
-            <form>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">SignUp</button>
-            </form>
-        </div>
+        <Container>
+            <WhitePawWrapper>
+                <Image src={whitepawImg} alt="white_paw" width={120} height={107.2} />
+            </WhitePawWrapper>
+
+            <SignUpTitle>회원가입</SignUpTitle>
+
+            <ContentContainer>
+                <SignUpButtonContainerColumn>
+                    <CustomButton color={BUTTON_COLORS.secondary} text="회원가입 하기" route="/login" />
+                </SignUpButtonContainerColumn>
+
+                <AlreadyMember>이미 회원이신가요?</AlreadyMember>
+                <GoLogin>로그인 하기</GoLogin>
+            </ContentContainer>
+        </Container>
     );
 };
 
 export default SignUp;
+
+const Container = styled.div`
+    background-color: ${BACKGROUND_COLORS.default};
+`;
+
+const WhitePawWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const SignUpTitle = styled.div`
+    color: ${TEXT_COLORS.title};
+    text-align: center;
+    font-family: ${FONTS.PRETENDARD[700]};
+    font-size: 1.5rem;
+`;
+
+const ContentContainer = styled.div`
+    ${PADDING_HORIZONTAL}
+    ${PADDING_VERTICAL}
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+`;
+
+const SignUpButtonContainerColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-top: 2rem;
+`;
+
+const AlreadyMember = styled.div`
+    color: #1f5a6e;
+    font-family: 'Noto Sans KR';
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`;
+
+const GoLogin = styled.div`
+    color: #1f5a6e;
+    font-family: 'Noto Sans KR';
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    text-decoration-line: underline;
+`;
